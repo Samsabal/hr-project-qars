99402b586f81fa2b8e27d98b8e0c2f38e084da45
LennertvS <57623736+LennertvS@users.noreply.github.com>
2020-11-16 13:20:59 +0100

    Test for pushing
    Pushing .......

--- QarsAngular/QarsAngular/Program.cs
+++ QarsAngular/QarsAngular/Program.cs
@@ -14,9 +14,9 @@ namespace QarsAngular
         public static string StringInput(string x)
         {
             Console.WriteLine(x);
-            string answer = Console.ReadLine(); //answer= Musab 
+            string answer = Console.ReadLine(); //answer=Musab 
             return answer;
-            //string test = Input("Username:") -> test = Lennert.
+            //string test = Input("Username:") -> test = Lennert........
         }
         public static int IntegerInput(string x)
         {
@@ -24,6 +24,26 @@ namespace QarsAngular
             int answer = Convert.ToInt32(Console.ReadLine()); //answer=19
             return answer;
             //int test = IntegerInput("Age:") -> test = 19.
+        }
+        public static string Location() 
+        {
+            Console.WriteLine("Where would you like to pick up your rental?/nPlease fill in your Province:             (don't forget capital letters)");
+            string x = Console.ReadLine();
+            string[] provincies = {"Groningen", "Friesland", "Drenthe", "Overijssel" };
+            bool CheckProvince = false;
+            for (int i=0; i<(provincies.Length-1); i++)
+            {
+                string y = provincies[i];
+                if(x == y) 
+                {
+                    CheckProvince = true;
+                }
+            }
+            if (CheckProvince)
+            {
+                return x;
+            }
+
         }
 
     }
