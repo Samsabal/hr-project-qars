ea0a4735ee397996ca2750b0be62b627076b242c
jasperschinkel <jasper-schinkel1999@hotmail.com>
2020-12-10 20:42:51 +0100

    Merge complete
    tweeked last pieces of code so it works fine.

---
+++ QarsAngular/QarsAngular/Migrations/20201210191952_InitialDB.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace QarsAngular.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "serviceidentification",
+                table: "ServiceBoekings",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Locations",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "zip",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "serviceidentification",
+                table: "ServiceBoekings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "zip",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
