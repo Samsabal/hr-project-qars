da57ccb7c62b90493a16ea251404840f737990c3
LennertvS <57623736+LennertvS@users.noreply.github.com>
2021-01-02 14:38:56 +0100

    Smallll fix
    No need to review this fix

--- QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
+++ QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'LoginComponent', //dit is nog niet helemaal correct, vraag hoe de selector moet staan.
+  selector: 'LoginComponent',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
