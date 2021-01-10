f82cf30661fefedfa80c53ac456126e6e96bc9fe
msbb010 <60715167+msbb010@users.noreply.github.com>
2021-01-10 16:23:01 +0100

    Revert "login with error"
    This reverts commit 4ad81155d5626cc2168867eb8ba65179b2b0d6dc.

--- QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
+++ QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
@@ -1,61 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
-import { AccountService, AlertService } from '@app/_services';
+@Component({
+  selector: 'LoginComponent',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
 
-@Component({ templateUrl: 'login.component.html' })
-export class LoginComponent implements OnInit {
-    form: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
+  loginForm;
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private accountService: AccountService,
-        private alertService: AlertService
-    ) { }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
 
-    ngOnInit() {
-        this.form = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
-        });
+  onSubmit(loginData) {
+    // Process checkout data here
+    this.loginForm.reset();
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.form.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // reset alerts on submit
-        this.alertService.clear();
-
-        // stop here if form is invalid
-        if (this.form.invalid) {
-            return;
-        }
-
-        this.loading = true;
-        this.accountService.login(this.f.username.value, this.f.password.value)
-            .pipe(first())
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+    console.warn('Uw gegevens zijn verstuurd!', loginData);
+  }
+   
 }
 
 
