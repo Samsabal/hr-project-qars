4ad81155d5626cc2168867eb8ba65179b2b0d6dc
msbb010 <60715167+msbb010@users.noreply.github.com>
2021-01-10 15:46:31 +0100

    login with error
    niet werkend

--- QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
+++ QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
@@ -1,29 +1,61 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'LoginComponent',
-  templateUrl: './login.component.html'
-})
-export class LoginComponent {
+import { AccountService, AlertService } from '@app/_services';
 
-  loginForm;
+@Component({ templateUrl: 'login.component.html' })
+export class LoginComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.loginForm = this.formBuilder.group({
-      username: '',
-      password: ''
-    });
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
 
-  onSubmit(loginData) {
-    // Process checkout data here
-    this.loginForm.reset();
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
 
-    console.warn('Uw gegevens zijn verstuurd!', loginData);
-  }
-   
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 }
 
 
