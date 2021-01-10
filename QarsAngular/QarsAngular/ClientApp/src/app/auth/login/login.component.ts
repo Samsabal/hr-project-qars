2ce3dfa7bdd4f4b3b71af616600109ead743244c
msbb010 <60715167+msbb010@users.noreply.github.com>
2021-01-10 16:42:47 +0100

    login 1/2
    stuurt data

--- QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
+++ QarsAngular/QarsAngular/ClientApp/src/app/auth/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'LoginComponent',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+  loginForm;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  onSubmit(loginData) {
+    // Process checkout data here
+    this.loginForm.reset();
+
+    console.warn('Uw gegevens zijn verstuurd!', loginData);
+  }
    
 }
-
 
